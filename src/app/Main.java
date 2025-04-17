d4bfd30dd600a3abc5360b6d800ee6614ea9a147
dovbysh1 <ydovbyshenko@gmail.com>
2025-04-17 16:17:47 +0300

    Add project info

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Converter Application");
+        System.out.println("Versoin 1.0.");
+        System.out.println("This application converts temperatures between Fahrenheit and Celsius");
+    }
+}
