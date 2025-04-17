090c88a62bd4fad2b4ecaf4887410e5a5059c6f4
dovbysh1 <ydovbyshenko@gmail.com>
2025-04-17 16:26:03 +0300

    Add Frt to Cls convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,5 +5,13 @@ public class Main {
         System.out.println("Converter Application");
         System.out.println("Versoin 1.0.");
         System.out.println("This application converts temperatures between Fahrenheit and Celsius");
+
+        double frt = 100;
+        double cls = convFrtToCls(frt);
+        System.out.println("Result is " + cls + " C ");
+    }
+
+    private static double convFrtToCls(double frt) {
+        return (frt - 32) * 5 / 9;
     }
 }
